9eb1cd18da75a7b65cc8d48832cba915f2dd0ade
hronchizhevoshice <hronchizhevoshice@gmail.com>
2025-10-19 13:49:01 +0700

    add questions

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -14,6 +14,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -40,8 +44,24 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+data class Question(
+    val text: String,
+    val answer: Boolean
+)
+
 @Composable
 fun GeoQuiz(modifier: Modifier = Modifier) {
+    val questions = listOf(
+        Question("Canberra is the capital of Australia.", true),
+        Question("The Pacific Ocean is larger than the Atlantic Ocean.", true),
+        Question("The Suez Canal connects the Red Sea and the Indian Ocean.", false),
+        Question("The source of the Nile River is in Egypt.", false),
+        Question("The Amazon River is the longest river in the Americas.", true),
+        Question("Lake Baikal is the world's oldest and deepest freshwater lake.", true)
+    )
+
+    var indexQuestion by remember { mutableStateOf(0) }
+
     Column(
         modifier = modifier.fillMaxSize()
     ) {
@@ -59,6 +79,19 @@ fun GeoQuiz(modifier: Modifier = Modifier) {
                 fontWeight = FontWeight.Bold,
                 color = Color.White,
                 modifier = Modifier.padding(start = 10.dp)
+            )
+        }
+        // question
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(25.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = questions[indexQuestion].text,
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center
             )
         }
     }
